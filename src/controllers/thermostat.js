afd967489d00ae5bcde1cfcbca00546a7e46c4fd
Jhonny Velasco <javelasco@outlook.com>
2021-05-16 23:06:03 -0500

    BackEnd uControl

--- src/controllers/thermostat.js
+++ src/controllers/thermostat.js
@@ -22,6 +22,71 @@ var thermostatController = {
         }
     },
 
+    tempLastDay: async function(req, res){
+        const {dateNow, deviceSerial} = req.body;
+
+        const endDate = dateNow.substring(0,10);
+        const startDate = endDate.substring(0,8).concat(Number(endDate.substring(8) - 1))
+
+        console.log('Fecha Recibida: ' + dateNow, 'startDate: ' + startDate, 'endDate: ' + endDate);
+
+        try {
+            const temp = await Temperature.find({$and: [{createdAt: {$gte: new Date(startDate)}}, {createdAt: {$lt: new Date(endDate)}}, {deviceSerial: deviceSerial}]}, {_id: 0, createdAt:0, updatedAt: 0, __v: 0});
+            
+            return res.status(200).send({
+                auth: true,
+                temp});
+            
+        } catch (error) {
+            return res.status(400).send({
+                auth: true,
+                message: "An error occured, please contact to Adminitrator!" });
+        }
+    },
+
+    tempLasthour: async function(req, res){
+        const {dateNow, deviceSerial} = req.body;
+
+        const endHour = dateNow;
+
+        console.log(endHour.substring(11,12));
+
+        if (endHour.substring(11,12) == '0'){
+            const startHour = endHour.substring(0,11).concat(Number(0)).concat(Number(endHour.substring(12,13)-1)).concat(endHour.substring(13));
+            console.log('Fecha Recibida: ' + dateNow, 'startHour: ' + startHour, 'endHour: ' + endHour);
+
+            try {
+                const temp = await Temperature.find({$and: [{createdAt: {$gte: startHour}}, {createdAt: {$lt: endHour}}, {deviceSerial: deviceSerial}]}, {_id: 0, updatedAt: 0, __v: 0});
+                
+                return res.status(200).send({
+                    auth: true,
+                    temp});
+                
+            } catch (error) {
+                return res.status(400).send({
+                    auth: true,
+                    message: "An error occured, please contact to Adminitrator!" });
+            }
+
+        } else {
+            const startHour = endHour.substring(0,11).concat(Number(endHour.substring(12,13)-1)).concat(endHour.substring(13));
+            console.log('Fecha Recibida: ' + dateNow, 'startHour: ' + startHour, 'endHour: ' + endHour);
+
+            try {
+                const temp = await Temperature.find({$and: [{createdAt: {$gte: startHour}}, {createdAt: {$lt: endHour}}, {deviceSerial: deviceSerial}]}, {_id: 0, createdAt:1, updatedAt: 0, __v: 0});
+                
+                return res.status(200).send({
+                    auth: true,
+                    temp});
+                
+            } catch (error) {
+                return res.status(400).send({
+                    auth: true,
+                    message: "An error occured, please contact to Adminitrator!" });
+            }
+
+        }
+    },
 }
 
 module.exports = thermostatController;
--- src/routes/thermostat.js
+++ src/routes/thermostat.js
@@ -7,5 +7,7 @@ const thermostatController = require('../controllers/thermostat');
 const verifyToken = require('../controllers/verifyToken');
 
 router.get ('/thermostat/temp', verifyToken, thermostatController.temp);
+router.get ('/thermostat/tempLastDay', verifyToken, thermostatController.tempLastDay);
+router.get ('/thermostat/tempLastHour', verifyToken, thermostatController.tempLasthour);
 
 module.exports = router;
